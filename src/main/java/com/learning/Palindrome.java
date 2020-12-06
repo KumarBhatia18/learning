ad3f3f5329408a9f4e82e60b71ca91af3936c61c
Bhatia <kumar.h.bhatia@accenture.com>
2020-12-06 18:21:27 +0530

    Initialize

---
+++ src/main/java/com/learning/Palindrome.java
@@ -0,0 +1,12 @@
+package com.learning;
+
+public class Palindrome {
+
+	public static void main(String args[]) {
+		String inputString = "Nitin";
+
+		for (int i = 0; i < inputString.length(); i++) {
+			System.out.println("String at index " + i + " is " + inputString.charAt(i));
+		}
+	}
+}
